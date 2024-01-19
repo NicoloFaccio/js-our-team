8d9c832d738a11a9d0dee89441bcf3f896f4b755
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-19 15:31:25 +0100

    created the array of the objects.

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,3 +6,41 @@
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+const players = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "src=./assets/img/wayne-barnett-founder-ceo.jpg", 
+    }, 
+
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "src=./assets/img/angela-caroll-chief-editor.jpg", 
+    }, 
+
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "src=./assets/img/walter-gordon-office-manager.jpg", 
+    }, 
+
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "src=./assets/img/angela-lopez-social-media-manager.jpg", 
+    }, 
+
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "src=./assets/img/scott-estrada-developer.jpg", 
+    }, 
+
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "src=./assets/img/barbara-ramos-graphic-designer.jpg", 
+    },
+]
